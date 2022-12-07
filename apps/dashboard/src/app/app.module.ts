6dbd73f3e5a7096f0afaf30dbf07034fa6a7075a
Andre Pinto <andrefjpinto@gmail.com>
2022-12-07 19:45:52 +0000

    add ng-bootstrap

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from '@angular/core';
+
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    NgbProgressbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
